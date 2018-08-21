4de973e46d420b68de923833e3cea46795c1dbe1
zero <zero.griffin@gmail.com>
2018-08-21 14:37:54 +0200

    Create objc protocol

---
+++ AudioPlayer/Source/Provider/ProviderProtocol.h
@@ -0,0 +1,13 @@
+//
+//  ProviderProtocol.h
+//  GstAudioPlayer
+//
+//  Created by zero on 21.08.18.
+//  Copyright © 2018 Blinkist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ProviderProtocol <NSObject>
+
+@end
